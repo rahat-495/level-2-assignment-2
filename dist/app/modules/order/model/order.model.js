9462216ca2853ecdc592a55437f8cf1b0d85bd7d
rahat-495 <md8702687@gmail.com>
2024-11-28 21:04:23 -0800

    dist folder are created for models !

--- dist/app/modules/order/model/order.model.js
+++ dist/app/modules/order/model/order.model.js
@@ -1,1 +1,24 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const orderSchema = new mongoose_1.default.Schema({
+    email: {
+        type: String,
+        required: [true, "Plz enter the email !"],
+    },
+    car: {
+        ref: 'car',
+        type: mongoose_1.default.Schema.ObjectId,
+        required: [true, "Plz enter the email !"],
+    },
+    quantity: {
+        type: Number,
+        required: [true, "Plz enter the quantity of the ordered car !"],
+    },
+    totalPrice: {
+        type: Number,
+    },
+});
