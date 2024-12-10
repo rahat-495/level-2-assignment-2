e25d73a47555cd93d842fa9dc14c7eff295fed3f
rahat-495 <md8702687@gmail.com>
2024-12-09 16:42:35 -0800

    app.use extra velidation are removed !

--- src/app.ts
+++ src/app.ts
@@ -11,43 +11,6 @@ app.use(express.json()) ;
 app.use('/' , carRoutes) ;
 app.use('/' , orderRoutes) ;
 
-app.use((err: any, req: Request, res: Response, next: NextFunction) : any => {
-
-    if (err.name === "ValidationError") {
-
-      const customErrors = Object.keys(err.errors).reduce((acc: any, key) => {
-        const { message, kind, path, value } = err.errors[key];
-        acc[key] = {
-          message: message,
-          name: err.errors[key].name,
-          properties: err.errors[key].properties,
-          kind: kind,
-          path: path,
-          value: value,
-        };
-        return acc;
-      }, {});
-  
-      return res.status(400).json({
-        message: "Validation failed",
-        success: false,
-        error: {
-          name: err.name,
-          errors: customErrors,
-        },
-        stack: err.stack, 
-      });
-
-    }
-  
-    res.status(500).json({
-      message: "Something went wrong",
-      success: false,
-      error: err.message,
-    });
-
-});
-
 app.use('/' , (req : Request , res : Response) => {
     res.json({message : "Server are connected !" , success : true}) ;
 })
